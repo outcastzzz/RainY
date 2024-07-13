74d38fd9695ccff2e8e92317ffd0dddd09cea904
outcastzzz <fleshka4895@gmail.com>
2024-07-13 05:15:28 +0400

    remove unused api way

--- data/src/main/java/com/example/data/network/ApiRoutes.kt
+++ data/src/main/java/com/example/data/network/ApiRoutes.kt
@@ -4,8 +4,6 @@ internal object ApiRoutes {
 
     private const val BASE_URL = "https://api.weatherapi.com/v1"
 
-    const val CURRENT_WEATHER = "$BASE_URL/current.json"
-
     const val FORECAST = "$BASE_URL/forecast.json"
 
     const val ASTRONOMY = "$BASE_URL/astronomy.json"
