8e2c2ddca82ec1d53623c7147a4a0ed92f22e2b3
outcastzzz <fleshka4895@gmail.com>
2024-07-14 05:26:03 +0400

    refactor list of hour view

--- feature/forecast/src/main/java/com/example/forecast/ForecastContent.kt
+++ feature/forecast/src/main/java/com/example/forecast/ForecastContent.kt
@@ -27,8 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.common.R
