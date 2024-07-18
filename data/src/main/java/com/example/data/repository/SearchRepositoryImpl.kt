73c57e0f7a05c2f7f6c0b5ba3d3acc123869c220
outcastzzz <fleshka4895@gmail.com>
2024-07-18 19:13:38 +0400

    refactor api request implementation

--- data/src/main/java/com/example/data/repository/SearchRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/SearchRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.example.data.repository
 
 import com.example.data.BuildConfig
+import com.example.data.network.ApiRoutes
+import com.example.domain.entity.City
 import com.example.domain.repository.SearchRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -13,14 +15,14 @@ class SearchRepositoryImpl @Inject constructor(
     private val client: HttpClient
 ): SearchRepository {
 
-    override suspend fun searchCityUseCase(q: String): List<com.example.domain.entity.City> {
-        return client.get("https://api.openweathermap.org/geo/1.0/direct") {
+    override suspend fun searchCityUseCase(q: String): List<City> {
+        return client.get(ApiRoutes.SEARCH_CITIES_ROUTE) {
             url {
                 parameters.append("q", q)
-                parameters.append("appid", BuildConfig.WEATHER_KEY)
+                parameters.append("api_key", BuildConfig.GEO_KEY)
             }
             contentType(ContentType.Application.Json)
-        }.body()
+        }.body<List<City>>().take(3)
     }
 
 }
