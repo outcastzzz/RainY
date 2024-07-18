0e773a2ac4b41eb6d22d970b3d45805dc14397ac
outcastzzz <fleshka4895@gmail.com>
2024-07-18 19:08:18 +0400

    refactor data model

--- domain/src/main/java/com/example/domain/entity/City.kt
+++ domain/src/main/java/com/example/domain/entity/City.kt
@@ -5,14 +5,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class City(
-    @SerialName("name")
-    val name: String,
-    @SerialName("lat")
     val lat: Float,
-    @SerialName("lon")
-    val long: Float,
-    @SerialName("country")
-    val country: String,
+    val lon: Float,
+    @SerialName("display_name")
+    val name: String,
 )
-
 
